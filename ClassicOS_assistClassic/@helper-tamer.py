e657767d3544d9bc2d7fe5da4c708ffb36fec682
Dmitriy Kopaygora <dmitriy.kopaygora@ab-soft.net>
2022-11-10 15:45:55 +0100

    add script

---
+++ ClassicOS_assistClassic/@helper-tamer.py
@@ -0,0 +1,23 @@
+#UO script: Helper for tamer
+#Run on ClassicAssist (ClassicUO client)
+
+UnsetAlias("Target4taming")
+if not FindAlias("Target4taming"):
+    PromptAlias("Target4taming")
+
+currentfolowers = Followers()
+while Followers() == currentfolowers:
+    Pathfind("Target4taming")
+    UseSkill("Animal Taming")
+    Target("Target4taming")
+    Pause(1000)
+    if InJournal('You have no chance of taming this creature'):
+        IgnoreObject(Target4taming)
+        Stop()
+    
+Rename("Target4taming", "oo")  
+# Release pet
+WaitForContext(animal_being_tamed, 10, 15000)
+WaitForGump(0x909cc741, 5000)
+ReplyGump(0x909cc741, 2)
+Pause(1000)
